9860626712667c70320081bc3506bc932ce332f2
Kamil Holubicki <kamil.holubicki@percona.com>
2024-10-01 15:27:35 +0200

    PXC-4463: PXC 8.0.39 refresh - Q3 2024
    https://perconadev.atlassian.net/browse/PXC-4463
    
    Fixed failing unit tests

--- galera/tests/certification_check.cpp
+++ galera/tests/certification_check.cpp
@@ -308,9 +308,12 @@ START_TEST(test_certification_trx_different_level_v4)
           galera::KeyData::BRANCH_KEY_TYPE,
           Certification::TEST_FAILED, {0}, 0},
         // 2)
+        // 3 depends on 1 if optimistic_pa=yes
+        // for PXC, by defult optimistic_pa=no, so
+        // 3 depends on 2
         { { {2, } }, 2, 2,
           { {void_cast("1"), 1}, {void_cast("1"), 1} }, 2, false,
-          3, 3, 2, 1, TrxHandle::F_BEGIN | TrxHandle::F_COMMIT,
+          3, 3, 2, 2, TrxHandle::F_BEGIN | TrxHandle::F_COMMIT,
           galera::KeyData::BRANCH_KEY_TYPE,
           Certification::TEST_OK, {0}, 0},
         { { {1, } }, 1, 1,
